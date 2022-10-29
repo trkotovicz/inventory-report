c7712087e613f1ac7f7a1fdc4d6a9960e30eaf26
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-10-28 22:30:33 -0300

    feat: implementa os métodos generate e empresa com mais produtos da classe SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,10 +1,18 @@
 from datetime import datetime
+from collections import Counter
 
 
 class SimpleReport:
-    @staticmethod
+    @classmethod
     def generate(self, list_products):
-        ...
+        fabricacao_mais_antiga = self.fabricacao_mais_antiga(list_products)
+        validade_mais_proxima = self.validade_mais_proxima(list_products)
+        empresa_mais_produtos = self.empresa_mais_produtos(list_products)
+        return (
+            f"Data de fabricação mais antiga: {fabricacao_mais_antiga}\n"
+            f"Data de validade mais próxima: {validade_mais_proxima}\n"
+            f"Empresa com mais produtos: {empresa_mais_produtos}"
+        )
 
     @classmethod
     def fabricacao_mais_antiga(self, list_products):
@@ -13,7 +21,7 @@ class SimpleReport:
 
     @classmethod
     def validade_mais_proxima(self, list_products):
-        today = datetime.now().strftime("%Y/%m/%d")
+        today = datetime.now().strftime("%Y-%m-%d")
         list = [
             product["data_de_validade"]
             for product in list_products
@@ -23,4 +31,8 @@ class SimpleReport:
 
     @classmethod
     def empresa_mais_produtos(self, list_products):
-        ...
+        list = [product["nome_da_empresa"] for product in list_products]
+        count = Counter(list).most_common(1)
+        return count
+        # count = Counter(list)
+        # return max(count)
