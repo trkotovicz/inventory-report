4a9cef6fbbe75b83777a5cdb6dc1829c78809775
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-10-31 18:40:45 -0300

    feat: implementa o método generate de CompleteReport

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,25 @@
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(self, list_products):
+        simple_report = super().generate(list_products)
+        list = [product["nome_da_empresa"] for product in list_products]
+        count = Counter(list)
+        complete_report = ""
+
+        for empresa, vezes in count.items():
+            complete_report += f"- {empresa}: {vezes}\n"
+
+        # empresa, vezes = count
+        # for empresa in count:
+        #     f"- {empresa}: {vezes}\n"
+        #     f"- {count}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{complete_report}"
+        )
