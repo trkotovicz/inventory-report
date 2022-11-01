957f20fce04a2fde5dd18d99fbee0345cb5398a2
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-11-01 15:42:17 -0300

    tests: refatora e implementa os testes corretamente da classe ReportDecorator

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,30 +1,37 @@
-import pytest
 from inventory_report.reports.colored_report import ColoredReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
-@pytest.fixture
-def mock():
-    return (
-        "Data de fabricação mais antiga: 10-05-2022\n"
-        "Data de validade mais próxima: 14-06-2021\n"
-        "Empresa com mais produtos: Farinini"
-    )
+RESET = "\033[0m"
+AZUL = "\033[36m"
+VERDE = "\033[32m"
+VERMELHO = "\033[31m"
+
+mock_produto = [
+    {
+        "id": 1,
+        "nome_do_produto": "produto x",
+        "nome_da_empresa": "empresa a",
+        "data_de_fabricacao": "2022-01-01",
+        "data_de_validade": "2022-12-12",
+        "numero_de_serie": 123456,
+        "instrucoes_de_armazenamento": "de qualquer jeito",
+    }
+]
+
+mock_fabricacao = f"{VERDE}Data de fabricação mais antiga:{RESET}"
+mock_validade = f"{VERDE}Data de validade mais próxima:{RESET}"
+mock_empresa = f"{VERDE}Empresa com mais produtos:{RESET}"
+mock_result_fabricacao = f"{AZUL}2022-01-01{RESET}"
+mock_result_validade = f"{AZUL}2022-12-12{RESET}"
+mock_result_empresa = f"{VERMELHO}empresa a{RESET}"
 
 
-mock_fabricacao = "\033[32mData de fabricação mais antiga: \033[0m"
-mock_validade = "\033[32mData de validade mais próxima: \033[0m"
-mock_empresa = "\033[32mEmpresa com mais produtos: \033[0m"
-mock_result_fabricacao = "\033[36m10-05-2022\033[0m"
-mock_result_validade = "\033[36m14-06-2021\033[0m"
-mock_result_empresa = "\033[31mFarinini\033[0m"
+def test_decorar_relatorio():
+    report = ColoredReport(SimpleReport).generate(mock_produto)
 
-
-def test_decorar_relatorio(mock):
-    report = SimpleReport(ColoredReport(mock))
-
-    assert report == (
-        f"{mock_fabricacao}{mock_result_fabricacao}"
-        f"{mock_validade}{mock_result_validade}"
-        f"{mock_empresa}{mock_result_empresa}"
-    )
+    assert (
+        f"{mock_fabricacao} {mock_result_fabricacao}\n"
+        f"{mock_validade} {mock_result_validade}\n"
+        f"{mock_empresa} {mock_result_empresa}"
+    ) in report
