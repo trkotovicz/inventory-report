45abcc03bd0b92a5ad76bf907c3717f01a2d947f
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-11-01 14:18:08 -0300

    tests: inicia os testes para ColoredReport

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,30 @@
-# from inventory_report.reports.colored_report import ColoredReport
+import pytest
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
-def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def mock():
+    return (
+        "Data de fabricação mais antiga: 10-05-2022\n"
+        "Data de validade mais próxima: 14-06-2021\n"
+        "Empresa com mais produtos: Farinini"
+    )
+
+
+mock_fabricacao = "\033[32mData de fabricação mais antiga: \033[0m"
+mock_validade = "\033[32mData de validade mais próxima: \033[0m"
+mock_empresa = "\033[32mEmpresa com mais produtos: \033[0m"
+mock_result_fabricacao = "\033[36m10-05-2022\033[0m"
+mock_result_validade = "\033[36m14-06-2021\033[0m"
+mock_result_empresa = "\033[31mFarinini\033[0m"
+
+
+def test_decorar_relatorio(mock):
+    report = SimpleReport(ColoredReport(mock))
+
+    assert report == (
+        f"{mock_fabricacao}{mock_result_fabricacao}"
+        f"{mock_validade}{mock_result_validade}"
+        f"{mock_empresa}{mock_result_empresa}"
+    )
