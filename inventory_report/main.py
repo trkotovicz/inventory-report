b5c04eef6f9adf63b7a203b3d734234efebefb45
Thais Kotovicz <93948349+trkotovicz@users.noreply.github.com>
2023-02-09 19:26:05 -0300

    feat: Implementa o método módulo main.py

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,24 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.importer.json_importer import JsonImporter
+
+
+def type_importer(path):
+    if path.endswith(".csv"):
+        return CsvImporter
+    elif path.endswith(".xml"):
+        return XmlImporter
+    elif path.endswith(".json"):
+        return JsonImporter
+
+
 def main():
-    pass
+    try:
+        path, report_type = sys.argv[1:]
+        importer = type_importer(path)
+        inventory = InventoryRefactor(importer)
+        sys.stdout.write(inventory.import_data(path, report_type), end="")
+    except IndexError:
+        sys.stderr.write("Verifique os argumentos\n")
