edd0fc4f54c52e56bb8611bc5f2f12798677c2d9
Thais Kotovicz <93948349+trkotovicz@users.noreply.github.com>
2023-02-09 19:06:44 -0300

    feat: Implementa a classe InventoryRefactor

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,21 @@
+from collections.abc import Iterable
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class InventoryRefactor(Iterable):
+    def __init__(self, importer):
+        self.importer = importer
+        self.data = []
+
+    def import_data(self, path, type):
+        self.data.extend(self.importer.import_data(path))
+
+        if type == "simples":
+            return SimpleReport.generate(self.data)
+        elif type == "completo":
+            return CompleteReport.generate(self.data)
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
